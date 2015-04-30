8b1d1b5d3decbc6078592aa8b85d017c9153e519
dreamwolf81 <s.assunti@gmail.com>
2015-04-30 10:28:32 +0200

    Commit

---
+++ WetSvc/install.bat
@@ -0,0 +1,4 @@
+﻿@echo off
+start "" /b /w %WINDIR%\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe WetSvc.exe
+start "" /b /w net start WetSvc
+
---
+++ WetSvc/uninstall.bat
@@ -0,0 +1,3 @@
+﻿@echo off
+start "" /b /w net stop WetSvc
+start "" /b /w %WINDIR%\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe /u WetSvc.exe
